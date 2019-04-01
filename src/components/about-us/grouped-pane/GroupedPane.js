c35893020fa59b7c39798757cc76db4d0d023508
Georgiy <georgiy.danielyan@herodigital.com>
2019-04-01 15:59:43 -0700

    revert back to classes for pane in order to hide second and third ones on mobile

--- src/components/about-us/grouped-pane/GroupedPane.js
+++ src/components/about-us/grouped-pane/GroupedPane.js
@@ -8,18 +8,32 @@ import './GroupedPane.scss';
 
 
 class GroupedPane extends Component {
+
+  componentWillUpdate() {
+    console.log('component will mount');
+  }
+
   render() {
+
+    const first = this.props.activeEmployee;
+    const second = this.props.employees[1];
+    const third = this.props.employees[2];
+
     return(
       <div className="grouped-pane">
         <Container fluid={true} className="panes">
           <Row>
-            { this.props.employees && 
-              this.props.employees.map((employee) => 
-                <Col xs={12} md={6} lg={4}>
-                  <Pane employee={employee} className="" />
-                </Col>
-              )
-            }
+            <Col xs={12} md={6} lg={4}>
+              <Pane employee={first} className="first-pane"/>
+            </Col>
+
+            <Col xs={12} md={6} lg={4}>
+              <Pane employee={second} className='second-pane' slide={this.props.slide}/>
+            </Col>
+
+            <Col xs={12} md={6} lg={4}>
+              <Pane employee={third} className='third-pane' slide={this.props.slide} />
+            </Col>
           </Row>
         </Container> 
       </div>
